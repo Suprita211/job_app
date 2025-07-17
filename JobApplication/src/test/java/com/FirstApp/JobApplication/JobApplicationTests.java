c3f97de728962289c8e8aead13475bf7402efb33
Suprita <suprita2026.com>
2025-07-17 12:29:39 +0530

    Set up with dependencies and connected to postgesql

---
+++ JobApplication/src/test/java/com/FirstApp/JobApplication/JobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FirstApp.JobApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
